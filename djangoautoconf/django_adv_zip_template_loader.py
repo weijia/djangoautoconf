acc2ba2dcebae64224e5292a28dc50c37bfd283b
Richard <richardwangwang@gmail.com>
2014-05-24 15:41:05 +0800

    Fix path generate issue ignore template not in zip file.

--- djangoautoconf/django_adv_zip_template_loader.py
+++ djangoautoconf/django_adv_zip_template_loader.py
@@ -11,6 +11,7 @@ from django.template.loader import BaseLoader
 
 
 # At compile time, cache the directories to search.
+
 fs_encoding = sys.getfilesystemencoding() or sys.getdefaultencoding()
 app_template_dirs = []
 for app in settings.INSTALLED_APPS:
@@ -29,8 +30,8 @@ def get_zip_file_and_relative_path(full_path_into_zip):
     full_path_into_zip = full_path_into_zip.replace("\\", "/")
     zip_ext = ".zip"
     zip_ext_start_index = full_path_into_zip.find(zip_ext + "/")
-    lib_path = full_path_into_zip[0:zip_ext_start_index]+ zip_ext
-    inner_path = full_path_into_zip[zip_ext_start_index+len(zip_ext)+2:]
+    lib_path = full_path_into_zip[0:zip_ext_start_index] + zip_ext
+    inner_path = full_path_into_zip[zip_ext_start_index+len(zip_ext)+1:]
     return lib_path, inner_path
 
 
@@ -39,26 +40,29 @@ log = logging.getLogger(__name__)
 
 class Loader(BaseLoader):
     is_usable = True
+
     def load_template_source(self, template_name, template_dirs=None):
         """Template loader that loads templates from zipped modules."""
         #Get every app's folder
         log.error("Calling zip loader")
         for folder in app_template_dirs:
-            lib_file, relative_folder = get_zip_file_and_relative_path(folder)
-            log.error(lib_file, relative_folder)
-            try:
-                z = zipfile.ZipFile(lib_file)
-                log.error(relative_folder+template_name)
-                template_path_in_zip = os.path.join(relative_folder, template_name).replace("\\", "/")
-                source = z.read(template_path_in_zip)
-            except (IOError, KeyError):
-                import traceback
-                log.error(traceback.format_exc())
-                continue
-            z.close()
-            # We found a template, so return the source.
-            template_path = "%s:%s" % (lib_file, template_path_in_zip)
-            return (source, template_path)
+            if ".zip/" in folder.replace("\\", "/"):
+                lib_file, relative_folder = get_zip_file_and_relative_path(folder)
+                log.error(lib_file, relative_folder)
+                try:
+                    z = zipfile.ZipFile(lib_file)
+                    log.error(relative_folder+template_name)
+                    template_path_in_zip = os.path.join(relative_folder, template_name).replace("\\", "/")
+                    source = z.read(template_path_in_zip)
+	            except (IOError, KeyError):
+	                import traceback
+	                log.error(traceback.format_exc())
+	                z.close()
+					continue
+				z.close()
+                # We found a template, so return the source.
+                template_path = "%s:%s" % (lib_file, template_path_in_zip)
+                return (source, template_path)
 
         # If we reach here, the template couldn't be loaded
         raise TemplateDoesNotExist(template_name)
