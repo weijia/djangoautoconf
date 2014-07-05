1491a38d897e31b74c30d58e00cd713e800509a3
Richard <richardwangwang@gmail.com>
2014-07-05 23:49:03 +0800

    Removed xadmin as it does not work with guardian.

--- djangoautoconf/auto_conf_admin_utils.py
+++ djangoautoconf/auto_conf_admin_utils.py
@@ -1,22 +1,23 @@
 from guardian.admin import GuardedModelAdmin
 
-#from django.contrib import admin
+# from django.contrib import admin
 import xadmin as admin
 
 
-def register_to_sys(class_inst, admin_class = None):
+def register_to_sys(class_inst, admin_class=None):
     if admin_class is None:
-        admin_class = type(class_inst.__name__+"Admin", (GuardedModelAdmin, ), {})
+        admin_class = type(class_inst.__name__ + "Admin", (GuardedModelAdmin, ), {})
     try:
         admin.site.register(class_inst, admin_class)
     except:
         pass
     try:
         from normal_admin.admin import user_admin_site
+
         user_admin_site.register(class_inst, admin_class)
     except:
         pass
-    #register(class_inst)
+        #register(class_inst)
 
 
 def register_all(class_list):
