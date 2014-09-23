85bf004f2889598791e815829fdc52a5d2f5bb54
Richard <richardwangwang@gmail.com>
2014-09-23 22:23:21 +0800

    Added oauth 2.0 authentication.

--- djangoautoconf/req_with_auth.py
+++ djangoautoconf/req_with_auth.py
@@ -1,6 +1,69 @@
 import json
+import logging
 from django.contrib.auth import authenticate, login
 from django_utils import retrieve_param
+from provider.oauth2.models import AccessToken
+from django.utils import timezone
+from provider.views import OAuthError
+
+
+class UserInactive(Exception):
+    pass
+
+
+class InvalidLogin(Exception):
+    pass
+
+
+class NoLoginInfo(Exception):
+    pass
+
+
+#Code from https://github.com/ianalexander/django-oauth2-tastypie
+def verify_access_token(key):
+    # Check if key is in AccessToken key
+    try:
+        token = AccessToken.objects.get(token=key)
+
+        # Check if token has expired
+        if token.expires < timezone.now():
+            raise OAuthError('AccessToken has expired.')
+    except AccessToken.DoesNotExist, e:
+        raise OAuthError("AccessToken not found at all.")
+
+    logging.info('Valid access')
+    return token
+
+
+def authenticate_req_throw_exception(request):
+    if not request.user.is_authenticated():
+        data = retrieve_param(request)
+        if 'consumer_key' in data:
+            try:
+                verify_access_token(data['consumer_key'])
+                return True
+            except OAuthError:
+                pass
+        if not (('username' in data) and ('password' in data)):
+            raise NoLoginInfo
+        username = data['username']
+        password = data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+            else:
+                raise UserInactive
+        else:
+            raise InvalidLogin
+    return True
+
+
+def authenticate_req(request):
+    try:
+        return authenticate_req_throw_exception(request)
+    except (UserInactive, InvalidLogin, NoLoginInfo):
+        return False
 
 
 class RequestWithAuth(object):
@@ -10,26 +73,19 @@ class RequestWithAuth(object):
         self.error_dict = {}
 
     def is_authenticated(self):
-        if ('username' in self.data) and ('password' in self.data):
-            username = self.data['username']
-            password = self.data['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(self.request, user)
-                    print 'login OK'
-                    return True
-                else:
-                    # Return a 'disabled account' error message
-                    print 'disabled account'
-                    self.error_dict = {"error": "disabled account"}
-            else:
-                # Return an 'invalid login' error message.
-                print 'invalid login'
-                self.error_dict = {"error": "invalid login"}
-
-                return False
-        self.error_dict = {"error": "no username and password"}
+        try:
+            authenticate_req_throw_exception(self.request)
+            print 'login OK'
+            return True
+        except UserInactive:
+            # Return a 'disabled account' error message
+            print 'disabled account'
+            self.error_dict = {"error": "disabled account"}
+        except InvalidLogin:
+            print 'invalid login'
+            self.error_dict = {"error": "invalid login"}
+        except NoLoginInfo:
+            self.error_dict = {"error": "no username and password"}
         return False
 
     def get_error_json(self):
@@ -44,18 +100,7 @@ try:
 
     class DjangoUserAuthentication(Authentication):
         def is_authenticated(self, request, **kwargs):
-            data = retrieve_param(request)
-            if not request.user.is_authenticated():
-                username = data['username']
-                password = data['password']
-                user = authenticate(username=username, password=password)
-                if user is not None:
-                    if user.is_active:
-                        login(request, user)
-                        return True
-            else:
-                return True
-            return False
+            return authenticate_req(request)
 
         # Optional but recommended
         def get_identifier(self, request):
