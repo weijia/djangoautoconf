c7af37a407a2cab7319f910830c6149addcde7d1
Richard <richardwangwang@gmail.com>
2014-10-09 21:50:39 +0800

    Fix tastypie resource name issue.

--- djangoautoconf/tastypie_utils.py
+++ djangoautoconf/tastypie_utils.py
@@ -1,15 +1,27 @@
 from tastypie.authorization import DjangoAuthorization
 from tastypie.resources import ModelResource
 from req_with_auth import DjangoUserAuthentication
+import re
 
 
-def create_tastypie_resource_class(class_inst):
+def class_name_to_low_case(class_name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', class_name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def create_tastypie_resource_class(class_inst, resource_name=None):
+    if resource_name is None:
+        resource_name = class_name_to_low_case(class_inst.__name__)
     resource_class = type(class_inst.__name__ + "Resource", (ModelResource, ), {
         "Meta": type("Meta", (), {
             "queryset": class_inst.objects.all(),
-            "resource_name": 'equip',
+            "resource_name": resource_name,
             "authentication": DjangoUserAuthentication(),
             "authorization": DjangoAuthorization(),
         })
     })
     return resource_class
+
+
+def create_tastypie_resource(class_inst):
+    return create_tastypie_resource_class(class_inst)()
