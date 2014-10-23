a29f48cbdad2a5cd67626fb0e79f137f45081d19
Richard <richardwangwang@gmail.com>
2014-10-23 09:45:32 +0800

    Added all filtering by default.

--- djangoautoconf/tastypie_utils.py
+++ djangoautoconf/tastypie_utils.py
@@ -1,4 +1,5 @@
 from tastypie.authorization import DjangoAuthorization
+from tastypie.constants import ALL
 from tastypie.resources import ModelResource
 from req_with_auth import DjangoUserAuthentication
 import re
@@ -12,13 +13,13 @@ def class_name_to_low_case(class_name):
 def create_tastypie_resource_class(class_inst, resource_name=None):
     if resource_name is None:
         resource_name = class_name_to_low_case(class_inst.__name__)
+    attributes = {"queryset": class_inst.objects.all(), "resource_name": resource_name,
+                  "authentication": DjangoUserAuthentication(), "authorization": DjangoAuthorization(),
+                  "filtering": {}}
+    for field in class_inst.__dict__['_meta'].fields:
+        attributes["filtering"].update({field.name: ALL})
     resource_class = type(class_inst.__name__ + "Resource", (ModelResource, ), {
-        "Meta": type("Meta", (), {
-            "queryset": class_inst.objects.all(),
-            "resource_name": resource_name,
-            "authentication": DjangoUserAuthentication(),
-            "authorization": DjangoAuthorization(),
-        })
+        "Meta": type("Meta", (), attributes)
     })
     return resource_class
 
