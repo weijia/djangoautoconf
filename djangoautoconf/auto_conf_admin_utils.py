73c0b08d1c63fa282385114cb00d6519160f893b
Richard <richardwangwang@gmail.com>
2015-01-11 17:28:59 +0800

    Added normal admin. Fixed duplicated parent when customize admin site.

--- djangoautoconf/auto_conf_admin_utils.py
+++ djangoautoconf/auto_conf_admin_utils.py
@@ -1,3 +1,4 @@
+import copy
 import inspect
 from django.conf import settings
 
@@ -13,27 +14,16 @@ from django.contrib import admin
 #import xadmin as admin
 
 
-def register_normal_admin(admin_class, class_inst):
-    # admin.site.register(class_inst)
-    try:
-        from normal_admin.admin import user_admin_site
-        if not (class_inst in user_admin_site):
-            user_admin_site.register(class_inst, admin_class)
-    except:
-        pass
-        #register(class_inst)
-
-
 def get_valid_admin_class(admin_class, class_inst):
     if admin_class is None:
         admin_class = type(class_inst.__name__ + "Admin", (SingleModelAdmin, ), {})
     return admin_class
 
 
-def register_admin(admin_class, class_inst):
+def register_admin(admin_class, class_inst, admin_site=admin.site):
     try:
-        if not (class_inst in admin.site._registry):
-            admin.site.register(class_inst, admin_class)
+        if not (class_inst in admin_site._registry):
+            admin_site.register(class_inst, admin_class)
     except Exception, e:
         if True:  # not (' is already registered' in e.message):
             print class_inst, admin_class
@@ -41,18 +31,28 @@ def register_admin(admin_class, class_inst):
             traceback.print_exc()
 
 
+def register_all_type_of_admin(admin_class, class_inst):
+    register_admin(admin_class, class_inst)
+    try:
+        from normal_admin.admin import user_admin_site
+
+        register_admin(admin_class, class_inst, user_admin_site)
+    except ImportError:
+        pass
+
+
 def register_to_sys(class_inst, admin_class=None):
     admin_class = get_valid_admin_class(admin_class, class_inst)
-    register_admin(admin_class, class_inst)
-    register_normal_admin(admin_class, class_inst)
+    register_all_type_of_admin(admin_class, class_inst)
 
 
 def get_valid_admin_class_with_list(admin_list, class_inst):
     #print admin_list
-    admin_list.append(SingleModelAdmin)
+    copied_admin_list = copy.copy(admin_list)
+    copied_admin_list.append(SingleModelAdmin)
     #print ModelAdmin
     #print final_parents
-    admin_class = type(class_inst.__name__ + "Admin", tuple(admin_list), {})
+    admin_class = type(class_inst.__name__ + "Admin", tuple(copied_admin_list), {})
     return admin_class
 
 
@@ -61,8 +61,7 @@ def register_to_sys_with_admin_list(class_inst, admin_list=None):
         admin_class = get_valid_admin_class_with_list([], class_inst)
     else:
         admin_class = get_valid_admin_class_with_list(admin_list, class_inst)
-    register_admin(admin_class, class_inst)
-    register_normal_admin(admin_class, class_inst)
+    register_all_type_of_admin(admin_class, class_inst)
 
 
 def register_all(class_list, admin_class_list=None):
