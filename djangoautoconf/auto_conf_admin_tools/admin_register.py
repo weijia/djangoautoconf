e6060a6b1a5cefb735b54dcd8a1652fbca2b0b40
Richard <richardwangwang@gmail.com>
2015-01-31 10:58:03 +0800

    Fix register issues.

--- djangoautoconf/auto_conf_admin_tools/admin_register.py
+++ djangoautoconf/auto_conf_admin_tools/admin_register.py
@@ -1,4 +1,5 @@
 import copy
+import inspect
 
 __author__ = 'q19420'
 from django.conf import settings
@@ -8,6 +9,17 @@ from django.contrib import admin
 if "guardian" in settings.INSTALLED_APPS:
     g_is_guardian_included = True
     from guardian.admin import GuardedModelAdmin
+
+
+def register_admin_without_duplicated_register(class_inst, admin_class, admin_site=admin.site):
+    try:
+        if not (class_inst in admin_site._registry):
+            admin_site.register(class_inst, admin_class)
+    except Exception, e:
+        if True:  # not (' is already registered' in e.message):
+            print class_inst, admin_class
+            import traceback
+            traceback.print_exc()
 
 
 class AdminRegister(object):
@@ -35,14 +47,33 @@ class AdminRegister(object):
         if not (list_display is None):
             self.admin_class_attributes["list_display"] = list_display
         if not (search_fields is None):
-            self.admin_class_attributes["list_display"] = search_fields
+            self.admin_class_attributes["search_fields"] = search_fields
         admin_class = self.get_valid_admin_class_with_list(class_inst)
-        admin.site.register(class_inst, admin_class)
+        register_admin_without_duplicated_register(class_inst, admin_class)
 
     def register_with_all_in_list_display(self, class_inst):
         self.admin_class_attributes["list_display"] = []
+        self.admin_class_attributes["search_fields"] = []
         for field in class_inst.__dict__['_meta'].fields:
             self.admin_class_attributes["list_display"].append(field.name)
+            self.admin_class_attributes["search_fields"].append(field.name)
         admin_class = self.get_valid_admin_class_with_list(class_inst)
-        admin.site.register(class_inst, admin_class)
-        self.admin_class_attributes = {}
+        register_admin_without_duplicated_register(class_inst, admin_class)
+
+    def class_enumerator(self, module_instance, exclude_name_list):
+        for name, obj in inspect.getmembers(module_instance):
+            if inspect.isclass(obj):
+                if name in exclude_name_list:
+                    continue
+                yield obj
+
+    def register_all_model(self, module_instance, exclude_name_list=[]):
+        """
+        :param module_instance: mostly the models module
+        :param exclude_name_list: class does not need to register or is already registered
+        :param admin_class_list:
+        :return: N/A
+        """
+        for class_instance in self.class_enumerator(module_instance, exclude_name_list):
+            self.register_with_all_in_list_display(class_instance)
+
