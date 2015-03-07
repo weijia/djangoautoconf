dc21c90fe81648c91c91cf669b3aa541aa5da583
Richard <richardwangwang@gmail.com>
2015-03-07 18:28:55 +0800

    Added userena and social auth.

---
+++ djangoautoconf/features/userena_settings.py
@@ -0,0 +1,26 @@
+__author__ = 'weijia'
+
+
+INSTALLED_APPS += (
+    'bootstrapform',
+    'userenabootstrap',
+    'userena',
+)
+
+
+AUTHENTICATION_BACKENDS = (
+    'userena.backends.UserenaAuthenticationBackend',
+    'guardian.backends.ObjectPermissionBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+AUTH_PROFILE_MODULE = 'webmanager.MyProfile'
+
+USERENA_SIGNIN_REDIRECT_URL = '/accounts/%(username)s/'
+LOGIN_URL = '/userena/signin/'
+LOGOUT_URL = '/userena/signout/'
+
+#EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
+USERENA_ACTIVATION_REQUIRED = False
+USERENA_SIGNIN_AFTER_SIGNUP = True
