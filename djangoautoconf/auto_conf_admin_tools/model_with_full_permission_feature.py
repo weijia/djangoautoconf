f64e2e0cf2d9e52266c12355a68fb39067b28eee
Richard <richardwangwang@gmail.com>
2015-08-29 15:06:36 +0800

    Added new admin features. Added admin in simple menu.

---
+++ djangoautoconf/auto_conf_admin_tools/model_with_full_permission_feature.py
@@ -0,0 +1,20 @@
+__author__ = 'weijia'
+
+
+def get_model_perms(self, request):
+    return {'add': True, 'change': True, 'delete': False}
+
+
+def has_change_permission(self, request, obj=None):
+    return True
+
+
+class ModelWithFullPermissionFeature(object):
+    def process_parent_class_list(self, parent_list, class_inst):
+        pass
+
+    def process_admin_class_attr(self, admin_attr, class_inst):
+        admin_attr["get_model_perms"] = get_model_perms
+        admin_attr["has_change_permission"] = has_change_permission
+        admin_attr["has_add_permission"] = has_change_permission
+
