eac57fec98e55085749a6fb0976b7a0873e786ce
Richard <richardwangwang@gmail.com>
2016-01-16 21:30:22 +0800

    Remove duplicated tuple items.

--- djangoautoconf/django_autoconf.py
+++ djangoautoconf/django_autoconf.py
@@ -88,7 +88,23 @@ class DjangoAutoConf(DjangoSettingManager):
         self.add_secret_key()
         self.update_installed_apps_etc()
         self.remove_empty_list()
+        self.refine_attrs(base_settings)
         dump_attrs(base_settings)
+
+    def remove_duplicated(self, value_in_tuple):
+        new_tuple = []
+        for i in value_in_tuple:
+            if not (i in new_tuple):
+                new_tuple.append(i)
+        return new_tuple
+
+    def refine_attributes(self, s):
+        for attr in dir(s):
+            if attr != attr.upper():
+                continue
+            value = getattr(s, attr)
+            if (type(value) is tuple):
+                setattr(s, attr, self.remove_duplicated(value))
 
     def check_params(self):
         if not os.path.exists(self.root_dir):
