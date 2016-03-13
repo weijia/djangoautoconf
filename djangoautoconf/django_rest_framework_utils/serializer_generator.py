0a172df410f067031c8799a18e9a05a9222d05ab
richard <richardwangwang@gmail.com>
2016-03-13 15:46:06 +0800

    Added django rest framework tools.

---
+++ djangoautoconf/django_rest_framework_utils/serializer_generator.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from rest_framework import generics
+
+
+def get_serializer(class_inst):
+    meta_class = type("Meta", tuple(), {"model": class_inst})
+    return type(class_inst.__name__ + "Serializer", tuple([serializers.ModelSerializer]),
+                {"Meta": meta_class}
+                )
+
+
+def get_api_class(class_inst, suffix="List", parent=[generics.ListCreateAPIView]):
+    serializer = get_serializer(class_inst)
+    return type(class_inst.__name__ + suffix, tuple(parent),
+                {
+                    "queryset": class_inst.objects.all(),
+                    "serializer_class": serializer,
+                }
+                )
+
+
+def get_create_api_class(class_inst):
+    return get_api_class(class_inst)
+
+
+def get_detail_api_class(class_inst):
+    return get_api_class(class_inst, "Detail", [generics.RetrieveUpdateDestroyAPIView])
