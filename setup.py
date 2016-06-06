06c0a0d9710ec414df4170a4b263ed58f70fd22d
Richard <richardwangwang@gmail.com>
2016-06-06 19:20:08 +0800

    Update version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='djangoautoconf',
-    version='0.6.10',
+    version='0.6.11',
     description='Create a package for ease setting django project settings.',
     long_description=readme + '\n\n' + history,
     author='Richard Wang',
