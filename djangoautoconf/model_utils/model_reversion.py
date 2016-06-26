83057af40cf16fbf038f610f97c19408beb0ff76
Richard <richardwangwang@gmail.com>
2016-06-26 23:46:58 +0800

    Fix for new django-reversion.

--- djangoautoconf/model_utils/model_reversion.py
+++ djangoautoconf/model_utils/model_reversion.py
@@ -12,6 +12,14 @@ def create_initial_version(obj):
         from reversion.revisions import add_to_revision
         add_to_revision(obj)
 
+
+def get_versioned_item(content_type, pk):
+    try:
+        return Version.objects.filter(content_type=content_type).filter(object_id_int=pk)
+    except:
+        return Version.objects.filter(content_type=content_type).filter(object_id=pk)
+
+
 global_save_signal_receiver = []
 
 
@@ -24,7 +32,9 @@ class PreSaveHandler(object):
         # logging.error("======================================")
         if not (instance.pk is None):
             content_type = ContentType.objects.get_for_model(self.model_inst)
-            versioned_pk_queryset = Version.objects.filter(content_type=content_type).filter(object_id_int=instance.pk)
+            # versioned_pk_queryset = Version.objects.filter(content_type=content_type).filter(
+            #     object_id_int=instance.pk)
+            versioned_pk_queryset = get_versioned_item(content_type, instance.pk)
             if not versioned_pk_queryset.exists():
                 item = self.model_inst.objects.get(pk=instance.pk)
                 try:
