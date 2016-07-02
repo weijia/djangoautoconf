fda4b3821e0ea9ce465af2401f894529dedeca44
Richard <richardwangwang@gmail.com>
2016-07-02 14:12:50 +0800

    Fix import issue.

--- djangoautoconf/model_utils/url_for_models.py
+++ djangoautoconf/model_utils/url_for_models.py
@@ -1,6 +1,6 @@
 from djangoautoconf.ajax_select_utils.channel_creator_for_model import add_channel_for_models_in_module
 from djangoautoconf.django_rest_framework_utils.serializer_generator import SerializerUrlGenerator
-from djangoautoconf.tastypie_utils import get_tastypie_urls
+from djangoautoconf.model_utils.tastypie_utils import get_tastypie_urls
 
 
 def add_all_urls(urlpatterns, models):
