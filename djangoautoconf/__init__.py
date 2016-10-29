6325b28eedfd049784d2d29e3573b99d544976c1
Richard <richardwangwang@gmail.com>
2016-10-29 15:13:11 +0800

    Update versions.

--- djangoautoconf/__init__.py
+++ djangoautoconf/__init__.py
@@ -3,6 +3,6 @@
 
 __author__ = 'Richard Wang'
 __email__ = 'richardwangwang@gmail.com'
-__version__ = '0.6.22'
+__version__ = '0.6.24'
 
 from django_autoconf import DjangoAutoConf
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='djangoautoconf',
-    version='0.6.22',
+    version='0.6.24',
     description='Create a package for ease setting django project settings.',
     long_description=readme + '\n\n' + history,
     author='Richard Wang',
