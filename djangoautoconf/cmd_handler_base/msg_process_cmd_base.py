b774712756eb8c005a03c496c17ccda935f3f05f
richard <richardwangwang@gmail.com>
2017-02-04 17:29:23 +0800

    Remove old arg parse.

--- djangoautoconf/cmd_handler_base/msg_process_cmd_base.py
+++ djangoautoconf/cmd_handler_base/msg_process_cmd_base.py
@@ -16,34 +16,38 @@ class DjangoCmdBase(BaseCommand):
     LOG_LEVELS = ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG']
 
     help = 'Help text here'
-    option_list = BaseCommand.option_list + (
-        # make_option('--duration',
-        #     action='store',
-        #     dest='duration',
-        #     type='int',
-        #     default=0,
-        #     help='Run task for this many seconds (0 or less to run forever) - default is 0'),
-        # make_option('--sleep',
-        #     action='store',
-        #     dest='sleep',
-        #     type='float',
-        #     default=5.0,
-        #     help='Sleep for this many seconds before checking for new tasks (if none were found) - default is 5'),
-        make_option('--log-file',
-                    action='store',
-                    dest='log_file',
-                    help='Log file destination'),
-        make_option('--log-std',
-                    action='store_true',
-                    dest='log_std',
-                    help='Redirect stdout and stderr to the logging system'),
-        make_option('--log-level',
-                    action='store',
-                    type='choice',
-                    choices=LOG_LEVELS,
-                    dest='log_level',
-                    help='Set logging level (%s)' % ', '.join(LOG_LEVELS)),
-    )
+    # option_list = BaseCommand.option_list + (
+    #     # make_option('--duration',
+    #     #     action='store',
+    #     #     dest='duration',
+    #     #     type='int',
+    #     #     default=0,
+    #     #     help='Run task for this many seconds (0 or less to run forever) - default is 0'),
+    #     # make_option('--sleep',
+    #     #     action='store',
+    #     #     dest='sleep',
+    #     #     type='float',
+    #     #     default=5.0,
+    #     #     help='Sleep for this many seconds before checking for new tasks (if none were found) - default is 5'),
+    #     make_option('--log-file',
+    #                 action='store',
+    #                 dest='log_file',
+    #                 help='Log file destination'),
+    #     make_option('--log-std',
+    #                 action='store_true',
+    #                 dest='log_std',
+    #                 help='Redirect stdout and stderr to the logging system'),
+    #     make_option('--log-level',
+    #                 action='store',
+    #                 type='choice',
+    #                 choices=LOG_LEVELS,
+    #                 dest='log_level',
+    #                 help='Set logging level (%s)' % ', '.join(LOG_LEVELS)),
+    # )
+
+    def __init__(self, stdout=None, stderr=None, no_color=False):
+        super(DjangoCmdBase, self).__init__(stdout, stderr, no_color)
+        self.options = None
 
     def _configure_logging(self, log_level, log_file, log_std):
 
@@ -73,9 +77,10 @@ class DjangoCmdBase(BaseCommand):
 
     def handle(self, *args, **options):
         # logging.basicConfig(level=logging.DEBUG)
-        log_level = options.pop('log_level', None)
-        log_file = options.pop('log_file', None)
-        log_std = options.pop('log_std', False)
+        # log_level = options.pop('log_level', None)
+        # log_file = options.pop('log_file', None)
+        # log_std = options.pop('log_std', False)
+        self.options = options
         # duration = options.pop('duration', 0)
         # sleep = options.pop('sleep', 5.0)
 
