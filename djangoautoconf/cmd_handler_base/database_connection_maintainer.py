6ed4aa25d5e7113df75a622f6e86e83e9ace1390
Richard <richardwangwang@gmail.com>
2017-08-28 20:35:30 +0800

    Update database close timeout value.

--- djangoautoconf/cmd_handler_base/database_connection_maintainer.py
+++ djangoautoconf/cmd_handler_base/database_connection_maintainer.py
@@ -5,18 +5,20 @@ from django.db import close_old_connections
 
 
 class DatabaseConnectionMaintainer(object):
+    DB_TIMEOUT_SECONDS = 5*60
+
     def __init__(self):
         self.clients = set()
         # self.device_to_protocol = {}
         self.is_recent_db_change_occurred = False
-        self.delay_and_execute(3600, self.close_db_connection_if_needed)
+        self.delay_and_execute(self.DB_TIMEOUT_SECONDS, self.close_db_connection_if_needed)
 
     def close_db_connection_if_needed(self):
         if not self.is_recent_db_change_occurred:
             close_old_connections()
             print "db connection closed"
         self.is_recent_db_change_occurred = False
-        self.delay_and_execute(3600, self.close_db_connection_if_needed)
+        self.delay_and_execute(self.DB_TIMEOUT_SECONDS, self.close_db_connection_if_needed)
 
     def refresh_timeout(self):
         self.is_recent_db_change_occurred = True
