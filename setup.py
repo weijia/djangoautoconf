3df2baa2305227d0c6b30bc2decdead34561ffbb
Richard <richardwangwang@gmail.com>
2018-11-21 22:28:00 +0800

    Recover MANIFEST.in. Add templates in MANIFEST.in. Update version. Remove package_data param in setup.py.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='djangoautoconf',
-    version='0.6.36',
+    version='0.6.37',
     description='Create a package for ease setting django project settings.',
     long_description=readme + '\n\n' + history,
     author='Richard Wang',
@@ -47,7 +47,7 @@ setup(
     ],
     package_dir={'djangoautoconf': 'djangoautoconf'},
     # include_package_data=True,
-    package_data={'djangoautoconf': ['templates/*']},
+    # package_data={'djangoautoconf': ['templates/*']},
     install_requires=[
         'django-extensions',
         'ufs-tools',
