e54053d27a677b0113c617220355382ba0542995
Richard <richard.j.wang@nokia-sbell.com>
2018-11-22 17:05:40 +0800

    Fix log path issue.

--- djangoautoconf/log_utils/log_path_utils.py
+++ djangoautoconf/log_utils/log_path_utils.py
@@ -4,7 +4,7 @@ from ufs_tools.folder_tool import ensure_dir
 
 
 def get_log_file_path(folder, log_file_name, ext=".log"):
-    log_folder_relative_path = 'logs\%s' % folder
+    log_folder_relative_path = os.path.join('logs', folder)
     log_filename = '%s%s' % (log_file_name, ext)
     current_dir = os.path.join(os.getcwd())
     folder_log_full_path = os.path.join(current_dir, log_folder_relative_path)
