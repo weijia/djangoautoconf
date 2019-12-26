5f28800e3aac5b9e694dc2234dcc1657fc8707ee
Richard <richard.j.wang@nokia-sbell.com>
2019-12-26 15:37:20 +0800

    Update codes to remove warnings in IDE

--- djangoautoconf/features/userena_and_python_social_auth_settings.py
+++ djangoautoconf/features/userena_and_python_social_auth_settings.py
@@ -2,6 +2,8 @@ from djangoautoconf.local_key_manager import ConfigurableAttributeGetter
 
 __author__ = 'weijia'
 
+if 'INSTALLED_APPS' not in locals()["__builtins__"]:
+    INSTALLED_APPS = []
 
 INSTALLED_APPS += (
     'bootstrapform',
@@ -11,8 +13,10 @@ INSTALLED_APPS += (
     'guardian',
 )
 
+if 'AUTHENTICATION_BACKENDS' not in locals()["__builtins__"]:
+    AUTHENTICATION_BACKENDS = []
 
-AUTHENTICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS += (
     'userena.backends.UserenaAuthenticationBackend',
     'social.backends.open_id.OpenIdAuth',
     'social.backends.google.GoogleOpenId',
@@ -20,8 +24,8 @@ AUTHENTICATION_BACKENDS = (
     'social.backends.google.GoogleOAuth',
     'social.backends.twitter.TwitterOAuth',
     'social.backends.yahoo.YahooOpenId',
-    #'social.backends.weibo.WeiboOAuth2',
-    #'social.backends.qq.QQOauth2',
+    # 'social.backends.weibo.WeiboOAuth2',
+    # 'social.backends.qq.QQOauth2',
     'social.backends.github.GithubOAuth2',
     'django.contrib.auth.backends.ModelBackend',
     'guardian.backends.ObjectPermissionBackend',
@@ -33,13 +37,15 @@ USERENA_SIGNIN_REDIRECT_URL = '/accounts/%(username)s/'
 LOGIN_URL = '/accounts/signin/'
 LOGOUT_URL = '/accounts/signout/'
 
-#EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 USERENA_ACTIVATION_REQUIRED = False
 USERENA_SIGNIN_AFTER_SIGNUP = True
 
 ANONYMOUS_USER_ID = -1
 
+if 'TEMPLATE_CONTEXT_PROCESSORS' not in locals()["__builtins__"]:
+    TEMPLATE_CONTEXT_PROCESSORS = []
 
 TEMPLATE_CONTEXT_PROCESSORS += (
     'social.apps.django_app.context_processors.backends',
@@ -60,6 +66,4 @@ github_api_secret = getter.get_attr("github_api_secret")
 SOCIAL_AUTH_GITHUB_KEY = github_api_key
 SOCIAL_AUTH_GITHUB_SECRET = github_api_secret
 
-
 SOCIAL_AUTH_WEIBO_DOMAIN_AS_USERNAME = True
-
